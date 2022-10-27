1129a64e222da0cd00da89754668938ba8076f67
ejemplo <email>
2022-10-26 19:44:56 -0500

    Comiendo

--- src/MainServer.java
+++ src/MainServer.java
@@ -1,3 +1,5 @@
+import modelo.Despachador;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -9,29 +11,22 @@ public class MainServer {
         int portNumber = Integer.parseInt(args[0]);
 
         try {
-                ServerSocket serverSocket = new ServerSocket(portNumber);
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
 
-                while(true) { // acepta un cliente y luego acepta al otro (while).
 
-                    Socket clientSocket = serverSocket.accept();
-                    PrintWriter out =
-                            new PrintWriter(clientSocket.getOutputStream(), true);
-                    BufferedReader in = new BufferedReader(
-                            new InputStreamReader(clientSocket.getInputStream()));
+                Despachador lector = new Despachador(clientSocket, "lector");
+                lector.start();
 
-                    String inputLine;
+                Despachador escritor = new Despachador(clientSocket, "escritor");
+                escritor.start();
 
-                    while((inputLine = in.readLine()) != null) {
-                        System.out.println(inputLine);
-                        if(inputLine.equals("Bye.")) {
-                            break;
-                        }
-                    }
+                //clientSocket.close();
+            }
 
-                }
-
-         } catch (Exception e) {
-            System.out.println("");
+        } catch (Exception e) {
+            System.out.println("Error>: " + e.getMessage());
         }
     }
 }
